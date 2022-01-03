3f8424000c98eb241bea9f922588eaaddd529760
alaa <alaaahmed8383@gmail.com>
2022-01-03 11:53:17 +0400

    Add project files.

---
+++ Test1/Areas/Employees/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Test1.Areas.Employees.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
